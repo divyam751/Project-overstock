1384b503fadb483c13c98df30381251f821db8e8
AnkitRawat@1998 <savankrawat@gmail.com>
2023-03-19 23:49:13 +0530

    done

---
+++ productpageall/productpage2.js
@@ -0,0 +1,280 @@
+var furnitureData = [
+
+   
+    {
+      id: "product31",
+      rating : "./images/4.1.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/03734d7463d2eab8b86443d490abf0a5fa866cf6/NNV-14%22-3500-lbs-Heavy-Duty-California-Bed-Frame%2C-Duty-Steel-Slat-Metal-Bed-Frame.jpg?imwidth=480&impolicy=medium",
+         
+         price : 4957.51,
+   
+         name: "mDesign Wide Dresser Storage Tower Organizer Unit, 5 Drawers",
+       
+         
+       },
+       {
+        id: "product32",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/9fb70a27d1a1e067f65a6f2ef2e0bfb98e0e525d/CraftPorch-2-Piece-Bedroom-Set-in-Luxurious-Velvet-Wingback-Panel.jpg?imwidth=480&impolicy=medium",
+         
+         price : 25498.04,
+   
+         name: "CraftPorch 2 Piece Bedroom Bench Set Velvet Wingback Upholstered Bed",
+       
+         
+       },
+       {
+        id: "product33",
+        rating : "./images/4.1.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/03734d7463d2eab8b86443d490abf0a5fa866cf6/NNV-14%22-3500-lbs-Heavy-Duty-California-Bed-Frame%2C-Duty-Steel-Slat-Metal-Bed-Frame.jpg?imwidth=480&impolicy=medium",
+         
+         price : 10831.22,
+   
+         name: "NNV 14 3500 lbs Heavy Duty Bed Frame, Duty Steel Slat Metal Bed Frame",
+       
+         
+       },
+       {
+        id: "product34",
+        rating : "./images/4.1.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/8893cb3d1b07d9308d652245df0eb679a7c68e48/Brookside-Kaylee-Adjustable-Upholstered-Headboard.jpg?imwidth=480&impolicy=medium",
+         
+         price : 7486.27 ,
+   
+         name: "Brookside Kaylee Adjustable Upholstered Headboard",
+       
+         
+       },
+       {
+        id: "product35",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/21d1fd6ad625bf8131c66bc90b8eaa3f87bb863f/Mid-Century-1-Drawer-Nightstand.jpg?imwidth=480&impolicy=medium",
+         
+         price : 16659.64 ,
+   
+         name: "Middlebrook Mid-Century Solid Wood 1-Drawer, 1 Shelf Nightstand",
+       
+         
+       },
+       {
+        id: "product36",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/2ebad0a4e7d94b6acf2bee922879c8f1ec5bda9d/Middlebrook-Gammelstaden-Mid-Century-Solid-Wood-6-Drawer-Dresser.jpg?imwidth=480&impolicy=medium",
+         
+         price : 28620.30 ,
+   
+         name: "Middlebrook Gammelstaden Mid-Century Solid Wood 6-Drawer Dresser",
+       
+         
+       },
+       {
+        id: "product37",
+        rating : "./images/4.4.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/cc7ff416cdefa87f42de64e9d11a422cca4c1457/Nathan-James-Harlow-Wall-Mount-Upholstered-Headboard-with-Metal-Rail.jpg?imwidth=480&impolicy=medium",
+         
+         price : 9667.80 ,
+   
+         name: "Nathan James Harlow Wall Mount Upholstered Headboard with Metal Rail",
+       
+         
+       },
+       {
+        id: "product38",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/1bdf1f904f3597aefee82cea29579f1d12cd9e40/100%25-Solid-Wood-Kyle-4-Door-Wardrobe%2C-White.jpg?imwidth=480&impolicy=medium",
+         
+         price : 95810.84,
+   
+         name: "Palace Imports 100% Solid Wood Kyle 4-Door Wardrobe Armoire with Raised Panel or Mirrored Doors",
+       
+         
+       },
+       {
+        id: "product39",
+        rating : "./images/4.8.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/bd8e7f43187fbc0d067b818a3071e7ae1d999646/Porch-%26-Den-Zoe-5-drawer-Chest.jpg?imwidth=480&impolicy=medium",
+         
+         price : 17849.48,
+   
+         name: "Porch & Den Zoe 5-drawer Chest",
+       
+         
+       },
+       {
+        id: "product40",
+        rating : "./images/3.5.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/6a126721e562d6cd9279e9d431e3a0c9985ad8cc/LUCID-Comfort-Collection-10-inch-Luxury-Gel-Memory-Foam-Mattress.jpg?imwidth=480&impolicy=medium",
+         
+         price : 20724.53,
+   
+         name: "LUCID Comfort Collection 10-inch Luxury Gel Memory Foam Mattress",
+       
+         
+       },
+       {
+        id: "product41",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/699413be95be54639d9590f5679eb9d0beeec30b/Middlebrook-Blaney-Solid-Wood-Spindle-Platform-Bed.jpg?imwidth=480&impolicy=medium",
+         
+         price : 34895.98,
+   
+         name: "Middlebrook Blaney Solid Wood Spindle Platform Bed",
+       
+         
+       },
+       {
+        id: "product42",
+        rating : "./images/4.4.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/91fa376ad2f4eeda9856a26b6acd299ae3fedf97/Middlebrook-Mid-Century-Solid-Wood-2-Drawer-Nightstand.jpg?imwidth=480&impolicy=medium",
+         
+         price : 11403.02 ,
+   
+         name: "Middlebrook Mid-Century Solid Wood 2-Drawer Nightstand",
+       
+         
+       },
+       {
+        id: "product43",
+        rating : "./images/4.4.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/af5ddc6fcc184dec7c2620206b60ff98981d4b29/Alazyhome-Simple-Vintage-Metal-Platform-Bed-Frame%2C-Easy-Assembly.jpg?imwidth=480&impolicy=medium",
+         
+         price : 15299.01 ,
+   
+         name: "Alazyhome Simple Vintage Metal Platform Bed Frame, Easy-Assembly",
+       
+         
+       },
+       {
+        id: "product44",
+        rating : "./images/4.6.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/e201ee73e78fd40c57eb4868be259913c0ac5ff9/Grain-Wood-Furniture-Shaker-Panel-Queen-Solid-Wood-Platform-Bed.jpg?imwidth=480&impolicy=medium",
+         
+         price : 33379.66 ,
+   
+         name: "Grain Wood Furniture Shaker Solid Wood Panel Platform Bed",
+       
+         
+       },
+       {
+        id: "product45",
+        rating : "./images/3.5.PNG",
+         image_url:
+         "https://ak1.ostkcdn.com/images/products/is/images/direct/de871b86dd5c5ceb681db1f0df1e203784413e99/Priage-by-ZINUS-9-Inch-Metal-Smart-BoxSpring%C2%AE-with-Quick-Assembly.jpg?imwidth=480&impolicy=medium",
+         
+         price : 11899.34 ,
+   
+         name: "Priage by ZINUS 9 Inch Metal Smart BoxSpring® with Quick Assembly",
+       
+         
+       },
+       
+     ];
+   
+     displayProducts();
+function displayProducts() {
+  furnitureData.map(function(elem) {
+    var div = document.createElement("div");
+    var imageRating = document.createElement("img");
+    imageRating.setAttribute("src", elem.rating);
+    imageRating.setAttribute("alt", elem.name);
+    imageRating.style.width = "100px";
+    imageRating.style.height = "20px";
+    imageRating.style.marginBottom = "10px";
+    var div = document.createElement("div");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.image_url);
+    image.setAttribute("alt", elem.name);
+
+    var priceBox = document.createElement("div");
+    var finalPrice = document.createElement("h5i");
+    finalPrice.textContent = "Sale Starts at INR " + elem.price;
+
+    var productName = document.createElement("p");
+    productName.textContent = elem.name;
+    productName.style.marginBottom = "10px"; // add margin between the name and the button
+
+    priceBox.append(finalPrice);
+
+    image.addEventListener("click", function() {
+      window.location.href = "productdetails.html?id=" + elem.id;
+    });
+    
+    productName.addEventListener("click", function() {
+      window.location.href = "productdetails.html?id=" + elem.id;
+    });
+
+    div.append(image, priceBox,imageRating, productName);
+    document.querySelector("#furData").append(div);
+  });
+}
+
+document.querySelector("#sortSelect").addEventListener("change", function() {
+  var sortValue = this.value;
+  var sortedData = sortProducts(furnitureData, sortValue);
+  document.querySelector("#furData").innerHTML = "";
+  displaySortedProducts(sortedData);
+});
+
+function sortProducts(data, sortValue) {
+  if (sortValue === "lowToHigh") {
+    return data.sort(function(a, b) {
+      return a.price - b.price;
+    });
+  } else if (sortValue === "highToLow") {
+    return data.sort(function(a, b) {
+      return b.price - a.price;
+    });
+  } else {
+    return data;
+  }
+}
+
+function displaySortedProducts(sortedData) {
+  sortedData.map(function(elem) {
+    var div = document.createElement("div");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.image_url);
+    image.setAttribute("alt", elem.name);
+    var div = document.createElement("div");
+    var imageRating = document.createElement("img");
+    imageRating.setAttribute("src", elem.rating);
+    imageRating.setAttribute("alt", elem.name);
+    imageRating.style.width = "100px";
+    imageRating.style.height = "20px";
+    imageRating.style.marginBottom = "10px";
+    var priceBox = document.createElement("div");
+    var finalPrice = document.createElement("h5i");
+    finalPrice.textContent = "Sale Starts at INR " + elem.price;
+
+    var productName = document.createElement("p");
+    productName.textContent = elem.name;
+    productName.style.marginBottom = "10px"; // add margin between the name and the button
+
+    priceBox.append(finalPrice);
+
+    image.addEventListener("click", function() {
+      window.location.href = "productdetails.html?id=" + elem.id;
+    });
+    
+    productName.addEventListener("click", function() {
+      window.location.href = "productdetails.html?id=" + elem.id;
+    });
+
+    div.append(image, priceBox, imageRating, productName);
+    document.querySelector("#furData").append(div);
+  });
+}
